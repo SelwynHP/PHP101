17f6d389440e30ff6bcee6a3653ef223a18012a2
SelwynHP <61285762+SelwynHP@users.noreply.github.com>
2020-03-07 23:02:25 -0500

    Fixed typo

--- data_display.php
+++ data_display.php
@@ -12,7 +12,7 @@
 	function array_man_init(){
 		$cars = array(
 		array("Year"=>2014,"Make"=>"Jeep","Model"=>"Grand-Cherokee","Price"=>48314,"KM"=>70000,"Color"=>"BLACK","Image"=>"Jeep.jpg"),
-		array("Year"=>2016,"Make"=>"BWM","Model"=>"X5-M","Price"=>48314,"KM"=>70000,"Color"=>"White","Image"=>"BMW.jpg"),
+		array("Year"=>2016,"Make"=>"BWM","Model"=>"X5-M","Price"=>126000,"KM"=>90,"Color"=>"White","Image"=>"BMW.jpg"),
 		array("Year"=>2015,"Make"=>"Acura","Model"=>"MDX","Price"=>60876,"KM"=>50000,"Color"=>"BLACK","Image"=>"ACURA.jpg"),
 		array("Year"=>2016,"Make"=>"Honda","Model"=>"Pilot","Price"=>46320,"KM"=>60,"Color"=>"Silver","Image"=>"Honda.jpg"),
 		array("Year"=>2015,"Make"=>"Audi","Model"=>"Q3","Price"=>30988,"KM"=>40000,"Color"=>"BLACK","Image"=>"Audi.jpg")
@@ -35,13 +35,13 @@
 			//Split line into tokens
 			$str = preg_split('/,/', $line);
 
-			//Trim not working
+			//Trim() data
 			/* foreach($str as $value){
 				$value = trim($value); 
 			} */
 
 			//Generating Collections
-			if(!(count($str) < 7)){
+			if(count($str) == 7){
 				$arr = array($header[0]=>$str[0],$header[1]=>$str[1],$header[2]=>$str[2],$header[3]=>$str[3],$header[4]=>$str[4],$header[5]=>$str[5],$header[6]=>$str[6]);
 				array_push($array,$arr);
 			}
